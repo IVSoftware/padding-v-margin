e5173f3c200b0cb929d35212fa36803dfe12dd41
Tom Gregor <tgregor@ivsoftware.com>
2023-02-03 10:06:07 -0700

    Add project files.

---
+++ padding-v-margin/MainForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace padding_v_margin
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.BackColor = System.Drawing.Color.White;
+            this.flowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel.Margin = new System.Windows.Forms.Padding(13, 13, 13, 13);
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            this.flowLayoutPanel.Padding = new System.Windows.Forms.Padding(13, 13, 13, 13);
+            this.flowLayoutPanel.Size = new System.Drawing.Size(778, 344);
+            this.flowLayoutPanel.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 344);
+            this.Controls.Add(this.flowLayoutPanel);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+    }
+}
+
---
+++ padding-v-margin/MainForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace padding_v_margin
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // Main form Margin in light blue
+            BackColor = Color.LightSkyBlue;
+            Padding = new Padding(25);
+
+            flowLayoutPanel.BackColor = Color.LightSalmon;
+            // Set these to 0 and let the individual controls
+            // manage the padding and margins.
+            flowLayoutPanel.Margin = new Padding(all: 0);
+            flowLayoutPanel.Padding = new Padding(all: 0);
+
+            for (int i = 0; i < 6; i++)
+            {
+                var panel = new TableLayoutPanel
+                {
+                    Name = $"panel{i}",
+                    Size = new Size(200, 100),
+
+                    // Margin 'outside' the panel will show in Light Salmon.
+                    // This will space the panels inside the FlowLayoutPanel
+                    Margin = new Padding(left: 10, top: 10, right: 0, bottom: 40),
+
+                    // The button inside this panel will have Padding around it.
+                    Padding = new Padding(all: 15),
+
+                    BackColor = Color.LightGreen,
+                    BackgroundImage = new Bitmap(
+                        Path.Combine(
+                            AppDomain.CurrentDomain.BaseDirectory,
+                            "Images",
+                            "back-image.png"
+                        )),
+                    BackgroundImageLayout = ImageLayout.Stretch,
+                };
+                // Add button to internal panel
+                var button = new Button
+                {
+                    Name = $"button{i}",
+                    Text = $"Button {i}",
+                    BackColor = Color.DarkSeaGreen,
+                    ForeColor = Color.WhiteSmoke,
+                    // By anchoring the button, it will autosize
+                    // respecting the Padding of its parent.
+                    Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Bottom,
+                    Margin = new Padding(all: 0),
+                    Padding = new Padding(all: 0),
+                };
+                panel.Controls.Add(button);
+
+                flowLayoutPanel.Controls.Add(panel);
+            }
+        }
+    }
+}
